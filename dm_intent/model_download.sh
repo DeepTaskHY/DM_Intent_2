aeb6e6fd4c8e73866b39a83b3082ee8cf48c510e
Eunsoo Lee <eunsoogi.lee@gmail.com>
2021-12-02 18:11:37 +0900

    Fix relative path issue

--- dm_intent/model_download.sh
+++ dm_intent/model_download.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-sudo apt-get install unzip
-sudo apt-get install curl
-echo "Model Download PATH : "pwd
-FILEID='1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0'
-FILENAME='hyu_intent.zip'
+MODELPATH=$(dirname $(realpath $0))
+FILEID="1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0"
+FILENAME="hyu_intent.zip"
+sudo apt-get install -y unzip curl
+echo "Model Download PATH : "${MODELPATH}
 curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
 rm /tmp/cookie
 rm -rf ckpt
 unzip $FILENAME
-mv hyu_intent/configuration.json dm_intent/
-mv hyu_intent/ckpt dm_intent/
+mv hyu_intent/configuration.json ${MODELPATH}
+mv hyu_intent/ckpt dm_intent/ ${MODELPATH}
 rm -r hyu_intent
 rm $FILENAME
 echo "Modelfile Download Complete!!"
-
-
--- dm_intent/model_download_checkFile.sh
+++ dm_intent/model_download_checkFile.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ ! -d dm_intent/ckpt ]; then
-  sudo apt-get install unzip
-  sudo apt-get install curl
-  echo "Model Download PATH : "pwd
-  FILEID='1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0'
-  FILENAME='hyu_intent.zip'
-  curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
-  curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
-  rm /tmp/cookie
-  rm -rf ckpt
-  unzip $FILENAME
-  mv hyu_intent/configuration.json .
-  mv hyu_intent/ckpt .
-  rm -r hyu_intent
-  rm $FILENAME
-  echo "Modelfile Download Complete!!"
-fi
+MODELPATH=$(dirname $(realpath $0))
 
+if [ ! -d ${MODELPATH}"/ckpt" ]; then
+    FILEID="1O_lDMEUMSZ7F0MbDHlw0T6FhQ6m66rm0"
+    FILENAME="hyu_intent.zip"
+    sudo apt-get install -y unzip curl
+    echo "Model Download PATH : "${MODELPATH}
+    curl -c /tmp/cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
+    curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' /tmp/cookie`&id="${FILEID} -o ${FILENAME}
+    rm /tmp/cookie
+    rm -rf ckpt
+    unzip $FILENAME
+    mv hyu_intent/configuration.json ${MODELPATH}
+    mv hyu_intent/ckpt ${MODELPATH}
+    rm -r hyu_intent
+    rm $FILENAME
+    echo "Modelfile Download Complete!!"
+fi
