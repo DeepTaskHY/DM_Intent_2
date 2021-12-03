76378b1fadfcb691bebf825f01b371c494bf0e0c
Eunsoo Lee <eunsoogi.lee@gmail.com>
2021-12-03 17:37:02 +0900

    Add configuration example

--- dm_intent/scripts/launcher.py
+++ dm_intent/scripts/launcher.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import json
-import rospkg
-import rospy, time
-from std_msgs.msg import String
-from intent_classifier import Model
-from google.protobuf import json_format
-import os, json
-from dtroslib.helpers import get_package_path, get_module_configuration, get_key_path
+
+import rospy
+import time
 from dtroslib.dialogflow import DialogflowClient
+from dtroslib.helpers import get_package_path, get_module_configuration, get_key_path
 from proto.marshal.collections.maps import MapComposite
+from std_msgs.msg import String
+
+from intent_classifier import Model
 
 test_path = get_package_path('dm_intent')
 # test_path = '..'
@@ -20,15 +20,15 @@ version = 'homecare'
 model = Model(test_path + '/model/model_ckpt.ckpt')
 publisher = rospy.Publisher('/taskCompletion', String, queue_size=10)
 
-
 configuration = get_module_configuration('dm_intent', version)
-
+project_id = configuration['project-id']
 key_path = get_key_path('dm_intent', configuration['authorization']['key'])
+language_code = configuration['language-code']
 
-dialogflow_client = DialogflowClient(project_id=configuration['project-id'],
-                                     session_id=configuration['session-id'],
+dialogflow_client = DialogflowClient(project_id=project_id,
                                      key_path=key_path,
-                                     language_code=configuration['language-code'])
+                                     language_code=language_code)
+
 
 def get_information(human_speech):
     response = dialogflow_client.detect_intent_text(human_speech)
@@ -36,7 +36,7 @@ def get_information(human_speech):
 
     for key, val in response.query_result.parameters.items():
         if isinstance(val, MapComposite):
-            val = dict(val) 
+            val = dict(val)
         elif type(val) != str:
             val = list(val)
         information[key] = val
@@ -58,7 +58,7 @@ def classify_intent(msg):
 
         out_msg = make_response_json(out, text, id, info)
         print(out_msg)
-    
+
         publisher.publish(json.dumps(out_msg))
 
 
